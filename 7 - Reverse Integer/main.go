43de6f85a06326af1b4826798bbd6a423bbaf127
AlecX <alecx@alecx.ru>
2017-11-13 20:34:43 +0300

    7 - Reverse integer

---
+++ 7 - Reverse Integer/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var num = 123
+	fmt.Println(num)
+}
+func reverse(x int) int {
+	var minus = false
+	var resultStr string
+	num := strconv.Itoa(x)
+	if x < 0 {
+		minus = true
+	}
+	if minus {
+		num = num[1:len(num)]
+	}
+
+	for _, v := range num {
+		resultStr = string(v) + resultStr
+	}
+
+	result, err := strconv.ParseInt(resultStr, 10, 32)
+	if err != nil {
+		return 0
+	}
+	if minus {
+		result = -result
+	}
+	return int(result)
+}
