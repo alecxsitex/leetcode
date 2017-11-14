f064e0cca9c2486e321440b2155eb9b4a7e7f0fb
alecx <alecx@alecx.ru>
2017-11-14 11:27:01 +0300

    Plus One

---
+++ Plus One/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num = []int32{0}
+	fmt.Println(plusOne(num))
+}
+
+func plusOne(digits []int) []int {
+	var l = len(digits)
+	digits[l-1]++
+
+	for i := l - 1; i >= 0; i-- {
+		if digits[i] < 10 {
+			break
+		} else {
+			if i-1 < 0 {
+				digits[i] = 1
+				digits = append(digits, 0)
+			} else {
+				digits[i-1]++
+				digits[i] = 0
+			}
+		}
+	}
+
+	return digits
+}
