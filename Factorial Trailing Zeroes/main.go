fb5e717380ac3fe6a94b064d54c69ab16ecb7324
alecx <alecx@alecx.ru>
2017-12-12 11:36:07 +0300

    Factorial Trailing Zeroes

---
+++ Factorial Trailing Zeroes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(trailingZeroes(30))
+}
+
+func trailingZeroes(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return n/5 + trailingZeroes(n/5)
+}
